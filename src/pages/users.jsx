e5f7a6b1c1bc5f1259b698b309b6349573d67b4c
unknown <sindhunandhu1999@gmail.com>
2022-05-17 15:15:48 +0530

    users

---
+++ src/pages/users.jsx
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Header from '../components/Header'
+
+function Users() {
+  const [users, setUsers] = useState([])
+  //fetching data from the server regarding bidders
+  const loadData = () => {
+    axios.get('http://localhost:8080/api/users').then((resp) => {
+      setUsers(resp.data)
+      console.log(Users)
+    })
+  }
+
+  useEffect(() => {
+    loadData()
+  }, [])
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-12'>
+            <h4 className='text-left p-2 border-bottom border-success'>
+              All Users
+            </h4>
+            <table className='table table-bordered table-light table-hover'>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Name</th>
+                  <th>Gender</th>
+                  <th>Password</th>
+                  <th>Userid</th>
+                  <th>Role</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((x) => (
+                  <tr key={x.id}>
+                    <td>{x.id}</td>
+                    <td>{x.uname}</td>
+                    <td>{x.gender}</td>
+                    <td>{x.password}</td>
+                    <td>{x.userid}</td>
+                    <td>{x.admin ? 'Admin' : 'User'}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Users
